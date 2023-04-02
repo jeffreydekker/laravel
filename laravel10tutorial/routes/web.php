eb1913614d8e0d02a128a8773c27e1b21b093fc2
Jeffrey Dekker <jeffreydekker@JDSNA.local>
2023-04-02 18:21:53 +0200

    learned crud functionality with query builder

--- laravel10tutorial/routes/web.php
+++ laravel10tutorial/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +16,59 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //      return a blade file as a page:
+    // return view('welcome');
+
+    // running raw SQL queries syntax:
+    // first, fetch all users:
+    // $users = DB::select("select * from users");
+
+    // create new user:
+    // $users = DB::insert('insert into users (username, email, password) values (?, ?, ?)', [
+    //     'John',
+    //     'john@doe.com',
+    //     'johndoe'
+    // ]);
+
+    // edit values in a table:
+    // $users = DB::update("update users set email='abc@alphabet.com' where id=2");
+
+    // delete a user:
+    // $users = DB::delete("delete from users where id=2");
+
+
+    // official database documentation: https://laravel.com/docs/10.x/database#running-an-insert-statement 
+    // dd($users);
+
+    //      using the query builder syntax:
+    // first, fetch all users:
+    // $users = DB::table('users')->get();
+
+    // create new user:
+    // $users = DB::table('users')->insert([
+    //     'username' => 'john',
+    //     'email' => 'johndoe@abc.com',
+    //     'password' => 'password'
+    // ]);
+
+    // edit values in a table:
+    // $users = DB::table('users')
+    // ->where('id', 3)
+    // ->update(['email' => 'random@email.com']);
+
+    // delete a user:
+    // $user = DB::table('users')
+    // ->where('id', 3)
+    // ->delete();
+
+
+    // official database documentation: https://laravel.com/docs/10.x/database#running-an-insert-statement 
+    // dd($users);
+
+    //      some powerful query builder tools: 
+    //      find, pluck, chunk, 
 });
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
